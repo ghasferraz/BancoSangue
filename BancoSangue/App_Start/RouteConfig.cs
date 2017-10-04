c659b862ce2d36c845cf3e6b1a925611d9695662
Gabriel Ribeiro Camargo Ferreira <gabrielxp@hotmail.com>
2017-10-04 11:15:02 -0300

    Aleração da rota e perfil de puplicação

--- BancoSangue/App_Start/RouteConfig.cs
+++ BancoSangue/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BancoSangue
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "Tela", action = "Index", id = UrlParameter.Optional }
 			);
 		}
 	}
